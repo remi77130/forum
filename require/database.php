5c48e35ea1d8b88e6b6b12504ba2d42948c9da89
Kévin GUIOT <contact@kevinguiot.fr>
2022-09-09 23:09:29 +0200

    Installation du nouveau système de config pour la base de données

--- require/database.php
+++ require/database.php
@@ -1,19 +1,14 @@
 <?php
+/*
+ * On vient récupérer le fichier où se trouvent les configurations pour se connecter à la DB
+ * 'require_once' fait le même rôle que include, mais est plus adapté pour ce cas-là (car on n'aura pas besoin de ré-include ce fichier, d'où le 'once')
+ */
 
+require_once 'config.php';
 
-try{
-
-       //Live mettre en commentaire pour travailler en local puis retirer pour envoyer en live
-       $bdd = new PDO('mysql:host=localhost;dbname=oemr6702_forum;charset=utf8;', 'oemr6702', 'WTDz-geeS-az4+');
-    
-       // Local mettre en commentaire pour envoyer en live 
-    //$bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
-
-} 
-
-
-
-catch(Exception $e){
-
-    die('Une erreur à été trouvée : ' . $e->getMessage());
+try {
+    $bdd = new PDO('mysql:host='.DB_HOST.';dbname='.DB_BASE.';charset=utf8;', DB_USER, DB_PASS);
+} catch(Exception $e){
+    die('Une erreur a été trouvée : ' . $e->getMessage());
 }
+?>
