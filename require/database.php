63fca321350fcdca263955fd3c1d76af3823490c
rémi.77 <96719910+remi77130@users.noreply.github.com>
2022-09-10 11:29:59 +0200

    fix conflict database.php

--- require/database.php
+++ require/database.php
@@ -6,27 +6,9 @@
 
 require_once 'config.php';
 
-<<<<<<< HEAD
-try{
-
-       //Live mettre en commentaire pour travailler en local puis retirer pour envoyer en live
-       //$bdd = new PDO('mysql:host=localhost;dbname=oemr6702_forum;charset=utf8;', 'oemr6702', 'WTDz-geeS-az4+');
-    
-       // Local mettre en commentaire pour envoyer en live 
-    $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
-
-} 
-
-
-
-catch(Exception $e){
-
-    die('Une erreur à été trouvée : ' . $e->getMessage());
-=======
 try {
     $bdd = new PDO('mysql:host='.DB_HOST.';dbname='.DB_BASE.';charset=utf8;', DB_USER, DB_PASS);
 } catch(Exception $e){
     die('Une erreur a été trouvée : ' . $e->getMessage());
->>>>>>> 1b2d82561b146f0d80dcad84845ee5e1c860e692
 }
 ?>
