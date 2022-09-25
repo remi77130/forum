4c158b49c2fd4eaf3bd5514dcf533aa75293efbe
Kévin GUIOT <contact@kevinguiot.fr>
2022-09-25 20:44:41 +0200

    Changement tab

--- option_profil.php
+++ option_profil.php
@@ -5,37 +5,22 @@
 <body>
 
 <div class="option_profil_user_connect">
-
     <div>
         <a href="editProfil.php">
-
-
-
             Editer mon profil
-
         </a>
     </div>
-
     <div>
         <a href="reception.php">Mes messages
-
-
-
-  <span class="notification">0</span>
-
+            <span class="notification">0</span>
         </a>
     </div>
-
     <div>
         <a href="deconnexion.php">Se déconnecter</a>
     </div>
-
     <div>
         <a href="profil_membre.php">Acceuil</a>
     </div>
-
 </div>
-
-
 </body>
 </html>
