1193d8074916de9164bbe4993f84e488058542a9
Kévin GUIOT <contact@kevinguiot.fr>
2022-09-27 00:24:10 +0200

    Rajout notification

--- option_profil.php
+++ option_profil.php
@@ -4,6 +4,22 @@
 </head>
 <body>
 
+<?php
+if (!empty($_SESSION)) {
+    $id_destinataire = $_SESSION['id'];
+
+    // On récupère les messages non-lus
+    $req_selectMessagesNonLus = $bdd->prepare('SELECT * FROM messages WHERE id_destinataire = :id_destinataire AND lu <> 1');
+    $req_selectMessagesNonLus->execute(
+        [
+            'id_destinataire' => $id_destinataire
+        ]
+    );
+} else {
+    exit;
+}
+?>
+
 <div class="option_profil_user_connect">
     <div>
         <a href="editProfil.php">
@@ -11,7 +27,9 @@
         </a>
     </div>
     <div>
-        <a href="reception.php">Mes messages</a>
+        <a href="reception.php">Mes messages
+            <span class="notification"><?= $req_selectMessagesNonLus->rowCount(); ?></span>
+        </a>
     </div>
     <div>
         <a href="deconnexion.php">Se déconnecter</a>
