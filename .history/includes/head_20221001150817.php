cf123d868d19d88f274a8b30a57686766f564ecc
rémi.77 <96719910+remi77130@users.noreply.github.com>
2022-10-03 16:53:08 +0200

    menu en cours

---
+++ .history/includes/head_20221001150817.php
@@ -0,0 +1,40 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<META NAME="SUBJECT"
+      CONTENT="chanderland pour discuter en live , rencontres et
+          discussion avec chanderland le meilleur tchat de france"/>
+
+<META NAME="ABSTRACT" CONTENT="Chat gratuit chanderland">
+
+<META NAME="KEYWORDS"
+      CONTENT="chanderland, chat, chat gratuit, tchat, chanderland chat, tchat gratuit, chanderland,
+          rencontre discrete"/>
+
+<META NAME="DESCRIPTION" CONTENT="chanderland est le premier chat gratuit de France :
+    tchater et voir des profils.
+le chat avec inscription rapide pour discuter avec des milliers de connectés "/>
+
+    <!-- Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="icones/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="icones/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="icones/favicon-16x16.png">
+<link rel="manifest" href="icones/favicon/site.webmanifest">
+<link rel="mask-icon" href="icones/favicon/safari-pinned-tab.svg" color="#8e8e8e">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+    <!-- Icon -->
+
+
+icon 2
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#00aba9">
+<meta name="theme-color" content="#ffffff">
+
+<TITLE>Chanderland, rencontre gratuite.</TITLE>
