e316c03ccb835946d4f1c1d227e4916429e0cd7c
rémi.77 <96719910+remi77130@users.noreply.github.com>
2023-01-12 09:37:06 +0100

    product.php

--- product.php
+++ product.php
@@ -4,50 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/product.scss">
     <title>Débloquer des options</title>
 </head>
 <body>
-    
-<div class="modal-dialog  modal-lg" role="document">
-		<div class="modal-content" id="intersticielDialog-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Fermer"><span style="font-size:0.9em">Fermer </span><div aria-hidden="true" class="fa fa-close" style="font-size:1.2em; vertical-align:bottom"></div></button>				<h4 class="modal-title" id="gridSystemModalLabel">Profitez à 100% de 
-                    <span><span>c</span> !</h4>
-			</div>
-			<div class="modal-body">
-				<div class="container-fluid" id="intersticielDialog">
-					<div class="col-xs-12 vcenter">
-<!-- H NON PREMIUM -->
-<div id="intersticiel" class="intersticiel intersticielHome IH728">
-<h5>Profitez à 100% de <span style="color: #404080;font-weight: bold;">liberti</span><span style="color: #c000c0;font-weight: bold;">c</span> : <a href="/myaccount/abonnement.php" class="linkbig"><strong>devenez Membre <span class="premium">PREMIUM</span></strong></a></h5>
-
-<br><br>
-<div class="text-center"><strong>De nouvelles femmes et de nouveaux couples recherchant des hommes<br> s'inscrivent chaque jour sur <span style="color: #404080;font-weight: bold;">liberti</span><span style="color: #c000c0;font-weight: bold;">c</span></strong></div>
-
-<br>
-<br>
-<div style="margin-left:10%;">
-<a href="/myaccount/abonnement.php"><strong>Devenez Membre <span class="premium">PREMIUM</span></strong></a> pour en profiter à 100% et faire de belles rencontres :<br><br>
-
-<img src="https://static-v2.libertic-cdn.com//i/puces/puce6_violet.gif" width="6" height="9" class="middle">  visualisez toutes les <i class="fa fa-picture-o"></i> photos et <i class="fa fa-film"></i> vidéos non floutées<br><br>
-<img src="https://static-v2.libertic-cdn.com//i/puces/puce6_violet.gif" width="6" height="9" class="middle"> informez les autres membres de votre passage en visitant simplement leur <i class="fa fa-user"></i> profil<br><br>
-<img src="https://static-v2.libertic-cdn.com//i/puces/puce6_violet.gif" width="6" height="9" class="middle"> envoyez et recevez des <i class="fa fa-kiss"></i> bisous virtuels<br><br>
-<img src="https://static-v2.libertic-cdn.com//i/puces/puce6_violet.gif" width="6" height="9" class="middle"> envoyez et recevez des <i class="fa fa-envelope-o"></i> messages<br><br>
-<img src="https://static-v2.libertic-cdn.com//i/puces/puce6_violet.gif" width="6" height="9" class="middle"> dialoguez en direct sur le <i class="fa fa-comments-o"></i> Chat texte/visio<br><br>
+<div class="product-card">
+    <div class="product-image">
+    <img src="icones/secure.svg">
+  </div>
+  <div class="product-details">
+    <h1>Etre certifié</h1>
+    <p>Vous permet d'avoir plus de chance de recevoir des message...</p>
+	<a href="https://buy.stripe.com/9AQ9Bea0T1RbboQfYY">Voir le produit</a>
+  </div>
 </div>
-<div class="text-center">
-	<a class="btn btn-default" href="/myaccount/abonnement.php"><strong>JE DEVIENS MEMBRE <span class="premium">PREMIUM</span></strong></a>
-</div>
-<br>
-<div class="text-right"><em><a data-dismiss="modal">Non merci, je verrai plus tard</a></em></div>
-</div>
-<!-- /H NON PREMIUM -->
-					</div>
-    	  		</div>
-			</div>
-		</div>
-	</div>
 
+<div class="product-card">
+  <div class="product-details">
+    <h1>Etre membre VIP</h1>
+    <p>Faite la difference avec un badge </p>
+	<a href="https://buy.stripe.com/cN214Ieh9dzTakMaEF">Prendre une carte privilége</a>
+</div>
+  <div class="product-image">
+    <img src="icones/diamond.svg">
+  </div>
+</div>
 
 </body>
 </html>
