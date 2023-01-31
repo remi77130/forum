1ce5b8578adf27095e595a9f98397dc5a30f1483
Julien Taront <julien.taront@gmail.com>
2023-01-31 14:41:18 +0100

    Fix: -Récupération de mot de passe.

--- sql/alter_20230112_1614.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `membres` ADD `confirmkey` VARCHAR(255) NOT NULL AFTER `mail`;
-ALTER TABLE `membres` ADD `confirme` INT(1) NOT NULL AFTER `Online` DEFAULT '0';
---
+++ sql/alter_20230131.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `recuperation` CHANGE `confirme` `confirme` INT(11) NOT NULL DEFAULT '0';
+ALTER TABLE `recuperation` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`id`);
